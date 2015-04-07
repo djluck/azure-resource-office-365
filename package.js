de7a0b4668f168c1e9e22015a6c9e3e51423f045
James Luck <djluck20@googlemail.com>
2015-04-07 16:20:38 +0100

    Updated package version and azure-active-directory dependency

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
     name: 'wiseguyeh:azure-resource-office-365',
     summary: "Allows users to authorize themselves against the office 365 rest APIs",
-    version: "0.1.0",
+    version: "0.1.1",
     git: "https://github.com/djluck/azure-resource-office-365"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1');
-  api.use('wiseguyeh:azure-active-directory@0.3.0', ['server']);
+  api.use(['wiseguyeh:azure-active-directory@0.3.1', 'underscore'], ['server']);
   api.addFiles('office365Resource.js', 'server');
 });
